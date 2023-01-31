1fc8589f08da177aeb1b234cbc6de275b25cc1fb
geckou-irei <taishi.irei@geckou.net>
2023-01-31 13:54:22 +0900

    :feat #6 carditemコンポーネント作成

--- pages/index.vue
+++ pages/index.vue
@@ -98,6 +98,7 @@ const fileUpload = (file: any) => {
           />
         </template>
       </SectionHeading>
+      <CardItem></CardItem>
     </ContentsContainer>
   </PageContainer>
 </template>
